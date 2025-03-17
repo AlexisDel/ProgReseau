092b651b6027d27c84b6023fab8bee648e6e1af5
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-03-17 14:19:36 +0100

    mqtt test

---
+++ mqtt/test_recp.py
@@ -0,0 +1,48 @@
+
+
+# python 3.11
+
+import random
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "python/ctrlrobot"
+# Generate a Client ID with the subscribe prefix.
+client_id = f'subscribe-{random.randint(0, 100)}'
+# username = 'emqx'
+# password = 'public'
+
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1,client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
