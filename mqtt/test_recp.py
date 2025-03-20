9f82245b20551c0a81f5eb0a2d04db26bc6e002f
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-03-20 10:53:47 +0100

    premier move

--- mqtt/test_recp.py
+++ mqtt/test_recp.py
@@ -33,9 +33,9 @@ def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
         print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
         if "1" in msg.payload.decode():
-            os.system(f"python3 ProgReseau/src/server/move.py 100 forward left 0.8")
+            os.system(f"python3 ProgReseau/src/server/move.py 100 forward no 0.8")
         elif "2" in msg.payload.decode():
-            os.system(f"python3 ProgReseau/src/server/move.py 100 forward right 0.8")
+            os.system(f"python3 ProgReseau/src/server/move.py 100")
         elif "3" in msg.payload.decode():
             os.system(f"python3 ProgReseau/src/server/move.py 100 forward no 0.8")
         else:
