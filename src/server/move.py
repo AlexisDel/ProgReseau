64926c28fce71b3ce4fa61d4d2897e57b319d865
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-03-24 11:26:16 +0100

    aaaaaaa

--- src/server/move.py
+++ src/server/move.py
@@ -58,8 +58,8 @@ def motorStop():#Motor stops
 # GPIO.setup(Motor_B_Pin2, GPIO.OUT)
 
 # motorStop()
-# pwm_A = GPIO.PWM(Motor_A_EN, 1000)
-# pwm_B = GPIO.PWM(Motor_B_EN, 1000)
+pwm_A = GPIO.PWM(Motor_A_EN, 1000)
+pwm_B = GPIO.PWM(Motor_B_EN, 1000)
 
 
 def motor_left(status, direction, speed):#Motor 2 positive and negative rotation
