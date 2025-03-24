edc3e78d1dda2b66bbf7412765cbb927674da169
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-03-24 10:52:57 +0100

    pin change for board

--- src/server/move.py
+++ src/server/move.py
@@ -12,13 +12,22 @@ import RPi.GPIO as GPIO
 # motor_EN_A: Pin7  |  motor_EN_B: Pin11
 # motor_A:  Pin8,Pin10    |  motor_B: Pin13,Pin12
 
-Motor_A_EN    = 4
-Motor_B_EN    = 17
+# Motor_A_EN    = 4
+# Motor_B_EN    = 17
 
-Motor_A_Pin1  = 14
-Motor_A_Pin2  = 15
-Motor_B_Pin1  = 27
-Motor_B_Pin2  = 18
+# Motor_A_Pin1  = 14
+# Motor_A_Pin2  = 15
+# Motor_B_Pin1  = 27
+# Motor_B_Pin2  = 18
+
+Motor_A_EN    = 7
+Motor_B_EN    = 11
+
+Motor_A_Pin1  = 8
+Motor_A_Pin2  = 10
+Motor_B_Pin1  = 13
+Motor_B_Pin2  = 12
+
 
 Dir_forward   = 0
 Dir_backward  = 1
@@ -41,20 +50,16 @@ def motorStop():#Motor stops
 	GPIO.output(Motor_B_EN, GPIO.LOW)
 
 
-#def setup():#Motor initialization
-#global pwm_A, pwm_B
-#GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(Motor_A_EN, GPIO.OUT)
-GPIO.setup(Motor_B_EN, GPIO.OUT)
-GPIO.setup(Motor_A_Pin1, GPIO.OUT)
-GPIO.setup(Motor_A_Pin2, GPIO.OUT)
-GPIO.setup(Motor_B_Pin1, GPIO.OUT)
-GPIO.setup(Motor_B_Pin2, GPIO.OUT)
+# GPIO.setup(Motor_A_EN, GPIO.OUT)
+# GPIO.setup(Motor_B_EN, GPIO.OUT)
+# GPIO.setup(Motor_A_Pin1, GPIO.OUT)
+# GPIO.setup(Motor_A_Pin2, GPIO.OUT)
+# GPIO.setup(Motor_B_Pin1, GPIO.OUT)
+# GPIO.setup(Motor_B_Pin2, GPIO.OUT)
 
-motorStop()
-pwm_A = GPIO.PWM(Motor_A_EN, 1000)
-pwm_B = GPIO.PWM(Motor_B_EN, 1000)
+# motorStop()
+# pwm_A = GPIO.PWM(Motor_A_EN, 1000)
+# pwm_B = GPIO.PWM(Motor_B_EN, 1000)
 
 
 def motor_left(status, direction, speed):#Motor 2 positive and negative rotation
