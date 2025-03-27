72e2dd7f4794c999c7b55673682fab07425b1b76
Maya <maya.soufan03@gmail.com>
2025-03-27 11:29:49 +0100

    ignore: cleanups

--- src/cam/test.py
+++ src/cam/test.py
@@ -1,37 +1,3 @@
-""" import tkinter as tk
-  
-# Create a GUI app 
-
-
-root = tk.Tk()
-root.title("Interface de Mouvement")
-root.geometry("1000x1000")
-  
-# Constructing the first frame, frame1 
-controls_frame = tk.Frame(root, width=3000, height=1000).grid(row=0, column=0) 
-controls_frame.pack(expand=True)
-
-# Displaying the frame1 in row 0 and column 0 
-  
-# Constructing the button b1 in frame1 
-btn_up = tk.Button(controls_frame, text="Haut")
-btn_left = tk.Button(controls_frame, text="Gauche")
-
-btn_right = tk.Button(controls_frame, text="Droite")
-btn_down = tk.Button(controls_frame, text="Bas")
-btn_extra = tk.Button(controls_frame, text="Extra")
-
-  
-# Constructing the second frame, frame2 
-video_frame = tk.Frame(root, bg="black")
-# Displaying the frame2 in row 0 and column 1 
-video_frame.grid(row=0, column=1) 
-#video_frame.pack(expand=True)
-# Constructing the button in frame2 
-
-# Make the loop for displaying app 
-root.mainloop()  """
-
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
@@ -57,18 +23,14 @@ root.title('Model Definition')
 root.resizable(width=FALSE, height=FALSE)
 root.geometry("1500x900")
 
-controls_frame = Frame(root, bg='green', width=400, height=900, padx=3, pady=3).grid(column = 1, row=0)
 video_frame = Frame(root, bg='black', width=1100, height=900, padx=3, pady=3).grid(column = 0, row=0)
 video_label = Label(video_frame, bg="black")
 video_label.grid(row=0, column=0, sticky="nsew")
-
-
-""" video_stream= HtmlFrame(video_frame)
-video_stream.load_website(VIDEO_URL) """
-
 cap = cv2.VideoCapture(VIDEO_URL)
-
 update_frame()
+
+controls_frame = Frame(root, bg='green', width=400, height=900, padx=3, pady=3).grid(column = 1, row=0)
+
 
 root.mainloop()
 
