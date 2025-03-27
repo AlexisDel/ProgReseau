af125e124f3ca96b59a1ba72a95ce013b49ade29
KoviazinaA <aleksandra.koviazina03@gmail.com>
2025-03-27 13:56:55 +0100

    try new pin

--- src/server/LED.py
+++ src/server/LED.py
@@ -21,7 +21,7 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 class LED:
     def __init__(self):
         self.LED_COUNT      = 12      # Number of LED pixels.
-        self.LED_PIN        = 23      # GPIO pin connected to the pixels (18 uses PWM!).
+        self.LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
         self.LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
         self.LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
         self.LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
