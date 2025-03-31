e44ef16f2bff62c1618be30b5932dba40c335cab
Maya <maya.soufan03@gmail.com>
2025-03-31 13:17:16 +0200

    fix: rasptank delete

--- rasptank/lib/python3.12/site-packages/adafruit_motor/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Scott Shawcroft for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
--- rasptank/lib/python3.12/site-packages/tkinterweb/tkhtml/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded combobox 2.3 [list source [file join $dir combobox-2.3.tm]]
